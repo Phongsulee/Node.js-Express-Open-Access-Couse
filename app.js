601b4b0c9305c60b7e1f5a334917c60288971f88
Phongsulee Rk. <74252857+Phongsulee@users.noreply.github.com>
2024-06-22 17:58:12 +0700

    date templates
    date templates all

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chalk = require('chalk')      //ให้ลงเป็น v.4.0.2 ไม่อย่างนั้นจะ run ไม่ได้
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) =>{
     res.send('Hello Torza Co,. Ltd. // GG_EZ_55');
