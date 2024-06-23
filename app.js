8ec60848be1e55de381a89849a6761855eb30b5e
Phongsulee Rk. <74252857+Phongsulee@users.noreply.github.com>
2024-06-23 09:47:59 +0700

    update 22

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk')      //ให้ลงเป็น v.4.0.2 ไม
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router()
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,6 +13,16 @@ app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine", "ejs");
+
+productRouter.route("/").get((req,res) =>{
+    res.send("Hello World !! I'm Product");
+});
+
+productRouter.route("/1").get((req,res) =>{
+    res.send("Hello World !! I'm Product-1");
+});
+
+app.user("/products", productRouter)
 
 app.get("/", (req,res) =>{
     //res.send('Hello Torza Co,. Ltd. // GG_EZ_55');
