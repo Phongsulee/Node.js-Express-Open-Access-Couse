68f430cd6c3306af618f373d5f6363606ad12654
Phongsulee Rk. <74252857+Phongsulee@users.noreply.github.com>
2024-06-27 15:12:06 +0700

    update-27-06-2004-003
    update-27-06-2004-003

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ app.set("view engine", "ejs");
 
 productRouter.route("/").get((req,res) =>{
     //res.send("Hello World !! I'm Torza-0");
-    res.render("products");
+    res.render("products",{
+        products: [
+            {productTitle:'น้ำยาล้างจาน 1', productDescription: 'น้ำยาล้างจานสูตร 1 ดีเลิศ', productPrice: 45},
+            {productTitle:'น้ำยาล้างจาน 2', productDescription: 'น้ำยาล้างจานสูตร 2 ดีเลิศ', productPrice: 55},
+            {productTitle:'น้ำยาล้างจาน 3', productDescription: 'น้ำยาล้างจานสูตร 3 ดีเลิศ', productPrice: 65},
+            {productTitle:'น้ำยาล้างจาน 4', productDescription: 'น้ำยาล้างจานสูตร 4 ดีเลิศ', productPrice: 75},
+        ]
+    });
 });
 
 productRouter.route("/1").get((req,res) =>{
