8b039ef1383d4453e51a390fbb40086b40cdcdb9
Phongsulee Rk. <74252857+Phongsulee@users.noreply.github.com>
2024-06-27 15:01:03 +0700

    update-27-06-2004-001
    update-27-06-2004-001

--- app.js
+++ app.js
@@ -3,33 +3,34 @@ const chalk = require('chalk')      //ให้ลงเป็น v.4.0.2 ไม
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const productRouter = express.Router()
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 productRouter.route("/").get((req,res) =>{
-    res.send("Hello World !! I'm Product");
+    res.send("Hello World !! I'm Torza-0");
 });
 
-productRouter.route("/1").get((req,res) => {
-    res.send("Hello World !! I'm Product-1");
+productRouter.route("/1").get((req,res) =>{
+    res.send("Hello World !! I'm Torza-1");
 });
 
-app.user("/products", productRouter)
 
-app.get("/", (req,res) =>{
+app.use("/products", productRouter)
+
+app.get("/", (req, res) => {
     //res.send('Hello Torza Co,. Ltd. // GG_EZ_55');
-    res.render('index',{username: 'Torza555', customers:["torza", "bon", "migo"]});
+    res.render('index', { username: 'Torza555', customers: ["torza", "bon", "migo"] });
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     // console.log("Listining on port" + chalk.red(" : "+port));            #คำสั่ง console.log
-    debug("Listining on port" + chalk.red(" : "+PORT));
+    debug("Listining on port" + chalk.red(" : " + PORT));
 })
